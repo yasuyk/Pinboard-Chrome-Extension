5f99ff373744ab27ded8ffa9e489fe166a7d98c1
Yasuyuki Oka <yasuyk@gmail.com>
2013-07-14 23:57:53 +0900

    Fix confition to check sender
    so sender.tab is optional.
    
    http://developer.chrome.com/extensions/runtime.html#type-MessageSender

--- src/javascripts/content_script.js
+++ src/javascripts/content_script.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     // 'use strict';
-    if (request.action && sender.tab) {
+    if (request.action && sender.id) {
       switch (request.action) {
       case 'getSelection':
         sendResponse(window.getSelection().toString());
